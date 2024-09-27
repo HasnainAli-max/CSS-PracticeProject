6072d67923dcf878908985f976067e75564533b2
HasnainAli-max <ha3710191@gmail.com>
2024-09-27 20:27:24 +0500

    Create a folder with two files, HTML and JavaScript, and apply some styling through JavaScript to implement a CSS toggle feature.

---
+++ CSS/ToggleCSS/script.js
@@ -0,0 +1,92 @@
+let a  =    document.getElementById('header')
+let b  =    document.getElementById('A-1')
+let c  =    document.getElementById('A-2')
+let d  =    document.getElementById('A-3')
+let e  =    document.getElementById('Welcome')
+let f  =    document.getElementById('Simple')
+let g  =    document.getElementById('btn-div')
+let h  =    document.getElementById('ToogleCSS')
+let i  =    document.getElementById('Copyrignt')
+
+
+
+
+
+
+function Addcss(){
+    a.style.height = '80px'
+    a.style.width = 'full'
+    a.style.backgroundColor = 'purple'
+    a.style.color = 'white'
+    a.style.textAlign = 'center'
+    a.style.padding = '50px'
+
+
+
+    b.style.textDecoration='none'
+    b.style.color='white'
+    b.style.margin='13px'
+
+
+
+    c.style.textDecoration='none'
+    c.style.color='white'
+    c.style.margin='13px'
+
+
+    d.style.textDecoration='none'
+    d.style.color='white'
+    d.style.margin='13px'
+
+    e.style.borderRadius='10px'
+    e.style.textAlign='center'
+
+
+    f.style.textAlign='center'
+
+    g.style.display='flex'
+    g.style.justifyContent='center'
+    g.style.marginTop='10px'
+
+
+    h.style.backgroundColor='purple'
+    h.style.color='white'
+    h.style.borderRadius='10px'
+    h.style.height='50px'
+    h.style.width='140px'
+    h.style.border='none'
+    h.innerText="Thank you 🎉"
+
+
+
+
+
+    i.style.backgroundColor='purple'
+    i.style.height='80px'
+    i.style.width='full'
+    i.style.display='flex'
+    i.style.textAlign='center'
+    i.style.alignItems='center'
+    i.style.justifyContent='center'
+    i.style.marginTop='50px'
+    i.style.color='white'
+    i.style.fontSize='large'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
